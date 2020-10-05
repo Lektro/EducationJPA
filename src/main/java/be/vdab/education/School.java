22b37988cdefbb20f536fdafc4e7d8d3a4e220f5
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-10-05 14:22:53 +0200

    forgot to fil in pom

--- src/main/java/be/vdab/education/School.java
+++ src/main/java/be/vdab/education/School.java
@@ -1,4 +1,12 @@
 package be.vdab.education;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class School {
+    private long id;
+    private String name;
+    private List<Student> students = new ArrayList<>();
+
 }
+
---
+++ src/main/java/be/vdab/education/Student.java
@@ -0,0 +1,4 @@
+package be.vdab.education;
+
+public class Student {
+}
